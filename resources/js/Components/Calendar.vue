ed3399f2bc479469ebd377ea0687b3068a506f21
Alex Nou <alexnou@gmail.com>
2024-07-27 00:22:50 -0700

    chore: update days between levels to 12

--- resources/js/Components/Calendar.vue
+++ resources/js/Components/Calendar.vue
@@ -13,7 +13,7 @@ const attributes = ref<any[]>([]);
 const startDate = new Date(2024, 3, 9);
 
 const buildAttributes = () => {
-    const dayInterval = 11;
+    const dayInterval = 12;
     for (let i = 1; i <= 60; i++) {
         attributes.value.push({
             key: `Goal: Level ${i} Complete`,
