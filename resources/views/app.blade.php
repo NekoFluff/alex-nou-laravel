48d7a94dcfbeaf2c193ff38153fcb3ea7687e691
NekoFluff <alexnou@gmail.com>
2026-01-21 15:16:19 -0700

    fix: favicon updates

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -54,9 +54,10 @@
     <meta name="twitter:site" content="@alexnou">
 
     <!-- Favicon -->
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon.png') }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="512x512" href="{{ asset('favicon.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('favicon.png') }}">
     <link rel="manifest" href="{{ asset('site.webmanifest') }}">
 
     <!-- Additional SEO -->
