b3c963745849db904e39ac442021cc1b240a1306
NekoFluff <alexnou@gmail.com>
2026-02-28 04:23:25 -0700

    Ignore health up checks in laravel nightwatch

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        $middleware->append(\App\Http\Middleware\ExcludeFromNightwatch::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
